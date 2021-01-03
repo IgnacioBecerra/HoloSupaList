53eb49d6e91dd7aaca724e0677d4203e82405d77
Ignacio Becerra <i1becerr@ucsd.edu>
2021-01-03 14:56:14 -0800

    now works with fork

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ const getVideoData = (list) => {
 // TODO watch streams that are happening within a few hours, wait until chat appears to run get video in setupObserver
 
 const scheduleObservers = () => {
-  var sql = `SELECT * FROM schedules where start_time > (NOW() - interval 3 hour) ORDER BY start_time;`;
+  var sql = `SELECT * FROM schedules where start_time > (NOW() - interval 1 hour) ORDER BY start_time;`;
   connection.query(sql, function (err, result) {
     if (err) throw err; // try catch
 
@@ -159,8 +159,7 @@ const updateSchedules = () => {
   });
 }
 
-//scheduleObservers();
-setupObserver('4IL7GsHnT0I')
+scheduleObservers();
 
 // update every 3 hours to account for changes
 setInterval( () => {
--- utils/dataScraper.js
+++ utils/dataScraper.js
@@ -17,7 +17,7 @@ options.addArguments("--start-maximized")
 
 options.addArguments('--headless')
 
-options.addArguments(['user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"']);
+options.addArguments([`user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"`]);
 
 options.addArguments('--disable-gpu')
 options.addArguments('--disable-dev-shm-usage')
