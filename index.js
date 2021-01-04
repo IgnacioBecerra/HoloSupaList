7bded5c73eca9b0f63e396fff29068235328a8ec
Ignacio Becerra <i1becerr@ucsd.edu>
2021-01-03 23:51:23 -0800

    back to now for initial select query

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ const getVideoData = (list) => {
 // TODO watch streams that are happening within a few hours, wait until chat appears to run get video in setupObserver
 
 const scheduleObservers = () => {
-  var sql = `SELECT * FROM schedules where start_time > (NOW() - interval 2 hour) ORDER BY start_time;`;
+  var sql = `SELECT * FROM schedules where start_time > NOW() ORDER BY start_time;`;
   connection.query(sql, function (err, result) {
     if (err) throw err; // try catch
 
