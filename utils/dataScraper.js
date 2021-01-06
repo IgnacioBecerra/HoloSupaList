1c70c803a253296bc9663be3de6c4fb3ad45e410
Ignacio Becerra <i1becerr@ucsd.edu>
2021-01-05 19:35:47 -0800

    created new class to contain instances

--- utils/dataScraper.js
+++ utils/dataScraper.js
@@ -28,8 +28,6 @@ let chromeCapabilities = webdriver.Capabilities.chrome();
 chromeCapabilities.set("acceptInsecureCerts", true);
 chromeCapabilities.set("acceptSslCerts", true);
 
-
-
 const {sanitizeObject, sanitizeString } = require('./sanitizers.js');
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
@@ -46,120 +44,127 @@ var db_config = {
   charset : 'utf8mb4'
 };
 
-const setupObserver = (videoId) => {
+class SuperchatScraper {
 
-   driver = new webdriver.Builder()
-             .withCapabilities(webdriver.Capabilities.chrome())
-             .setChromeOptions(options)
-             .build();
+  constructor(videoId, channelName, videoTitle) {
+    this.videoId = videoId;
+    this.channelName = channelName;
+    this.videoTitle = videoTitle;
+    this.setupObserver()
+  }
+  
+  setupObserver() {
+
+     driver = new webdriver.Builder()
+               .withCapabilities(webdriver.Capabilities.chrome())
+               .setChromeOptions(options)
+               .build();
 
 
-  driver.get(`https://www.youtube.com/live_chat?v=${videoId}`);
-  driver.executeScript('return navigator.userAgent').then((e) => {
-    console.log(e)
-  })
+    driver.get(`https://www.youtube.com/live_chat?v=${this.videoId}`);
+    driver.executeScript('return navigator.userAgent').then((e) => {
+      console.log(e)
+    })
 
 
-  driver.wait(until.elementLocated(By.css('#items.yt-live-chat-item-list-renderer')));
+    driver.wait(until.elementLocated(By.css('#items.yt-live-chat-item-list-renderer')));
 
 
-  let obs = `
-    let chatWindow = document.querySelector('#items.yt-live-chat-item-list-renderer');
-    let chatStopped = false;
+    let obs = `
+      let chatWindow = document.querySelector('#items.yt-live-chat-item-list-renderer');
+      let chatStopped = false;
 
-    let obs = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
+      let obs = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
 
-        // this running means messages are still appearing
-        chatStopped = false;
+          // this running means messages are still appearing
+          chatStopped = false;
 
-        const nodes = Array.from(mutation.addedNodes)
-        nodes.forEach(node => {
-          if (node instanceof HTMLElement && node.tagName == 'YT-LIVE-CHAT-PAID-MESSAGE-RENDERER') {
+          const nodes = Array.from(mutation.addedNodes)
+          nodes.forEach(node => {
+            if (node instanceof HTMLElement && node.tagName == 'YT-LIVE-CHAT-PAID-MESSAGE-RENDERER') {
 
-            let currentList = window.localStorage.getItem('chat');
+              let currentList = window.localStorage.getItem('chat');
 
-            let superChat = {
-              author: node.querySelector('#author-name').innerText,
-              amount: node.querySelector('#purchase-amount-column').innerText,
-              message: node.querySelector('#message').innerText,
-              timestamp: node.querySelector('#timestamp').innerText,
-              color: node.style.cssText.split(/[:;]/)[1],
+              let superChat = {
+                author: node.querySelector('#author-name').innerText,
+                amount: node.querySelector('#purchase-amount-column').innerText,
+                message: node.querySelector('#message').innerText,
+                timestamp: node.querySelector('#timestamp').innerText,
+                color: node.style.cssText.split(/[:;]/)[1],
+              }
+
+              console.log(superChat)
+
+              if(currentList) {
+
+                let list = JSON.parse(currentList);
+                list.push(superChat);
+
+                window.localStorage.setItem('chat', JSON.stringify(list));
+                console.log(node.querySelector('#content > #message').innerText);
+
+              } else {
+                window.localStorage.setItem('chat', JSON.stringify([superChat]));
+                console.log(node.querySelector('#content > #message').innerText);
+              }
             }
-
-            console.log(superChat)
-
-            if(currentList) {
-
-              let list = JSON.parse(currentList);
-              list.push(superChat);
-
-              window.localStorage.setItem('chat', JSON.stringify(list));
-              console.log(node.querySelector('#content > #message').innerText);
-
-            } else {
-              window.localStorage.setItem('chat', JSON.stringify([superChat]));
-              console.log(node.querySelector('#content > #message').innerText);
-            }
-          }
+          })
         })
       })
-    })
-  obs.observe(chatWindow, { childList: true });
+    obs.observe(chatWindow, { childList: true });
 
-  // Check every minute if chat has stopped. If so, disconnect.
-  let interval = setInterval( checkMessages, 60000);
-
-
-  function checkMessages() {
-    chatStopped = true;
-
-    // Wait to see if observer still has messages appearing
-    setTimeout( () => {
-      if(chatStopped) {
-        obs.disconnect();
-        clearInterval(interval);
-        console.log('disconnect');
-        window.localStorage.setItem('stopped', true);
-      }
-    }, 10000)}
-  `
-
-  driver.executeScript(obs);
-}
+    // Check every minute if chat has stopped. If so, disconnect.
+    let interval = setInterval( checkMessages, 60000);
 
 
-const insertData = (channel, videoTitle) => {
+    function checkMessages() {
+      chatStopped = true;
 
-  setInterval( () => {
-    driver.executeScript(`return window.localStorage.getItem('chat')`).then( list => {
-      let supas = JSON.parse(list);
+      // Wait to see if observer still has messages appearing
+      setTimeout( () => {
+        if(chatStopped) {
+          obs.disconnect();
+          clearInterval(interval);
+          console.log('disconnect');
+          window.localStorage.setItem('stopped', true);
+        }
+      }, 10000)}
+    `
 
-      if(!supas) return;
+    driver.executeScript(obs);
+    this.insertData()
+  }
 
-      supas.forEach((s) => {
-        sanitizeObject(s);
+  insertData() {
 
-        var sql = `INSERT INTO ${channel} (author, amount, message, timestamp, color, video) VALUES ("${s.author}", "${s.amount}", "${s.message}", "${s.timestamp}", "${s.color}", "${sanitizeString(videoTitle)}")`;
-        connection.query(sql, function (err, result) {
-          if (err) throw err; // try catch
-          console.log(s);
-        });
+    setInterval( () => {
+      driver.executeScript(`return window.localStorage.getItem('chat')`).then( list => {
+        let supas = JSON.parse(list);
+
+        if(!supas) return;
+
+        supas.forEach((s) => {
+          sanitizeObject(s);
+
+          var sql = `INSERT INTO ${this.channelName} (author, amount, message, timestamp, color, video) VALUES ("${s.author}", "${s.amount}", "${s.message}", "${s.timestamp}", "${s.color}", "${sanitizeString(this.videoTitle)}")`;
+          connection.query(sql, function (err, result) {
+            if (err) throw err; // try catch
+            console.log(s);
+          });
+        })
+
+        driver.executeScript(`window.localStorage.removeItem('chat')`);
       })
 
-      driver.executeScript(`window.localStorage.removeItem('chat')`);
-    })
+      driver.executeScript(`return window.localStorage.getItem('stopped')`).then( chatStopped => {
+        if(chatStopped) {
+          driver.close();
+        }
+      })
 
-    driver.executeScript(`return window.localStorage.getItem('stopped')`).then( chatStopped => {
-      if(chatStopped) {
-        driver.close();
-      }
-    })
-
-  }, 20000)
+    }, 20000)
+  }
 }
 
-module.exports = {
-  setupObserver,
-  insertData
-}
+module.exports = SuperchatScraper
