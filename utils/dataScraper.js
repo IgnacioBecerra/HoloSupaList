320aec92d4de3171c061f86274346f8b77b09d6d
Ignacio Becerra <i1becerr@ucsd.edu>
2021-01-10 15:49:08 -0800

    GOD

--- utils/dataScraper.js
+++ utils/dataScraper.js
@@ -15,7 +15,7 @@ options.addArguments("--proxy-bypass-list=*")
 options.addArguments("--start-maximized")
 
 
-//options.addArguments('--headless')
+options.addArguments('--headless')
 
 options.addArguments([`user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"`]);
 
